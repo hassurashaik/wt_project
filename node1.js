4fd2c3708978552276117fa1ef5becf2d13f5a0e
hassurashaik <130497782+hassurashaik@users.noreply.github.com>
2024-06-02 22:36:05 +0530

    Add files via upload
    adding node1

---
+++ node1.js
@@ -0,0 +1,10 @@
+const fs=require('fs');
+const http=require('http');
+const file=fs.readFileSync('final.html');
+const server=http.createServer((req,res)=>{
+    res.writeHead(200,{'content-type':'text/html'});
+    res.end(file);
+});
+server.listen(80,'127.0.0.1',()=>{
+    console.log('listening on port 80');
+});
